7d0b5e3b0c5ea90fe31ec754f8123a20b3d4d4ef
spara <sparanofrancesco@hotmail.it>
2019-09-28 11:59:09 +0200

    update

--- Restaurant/src/app/SaveNotice.java
+++ Restaurant/src/app/SaveNotice.java
@@ -43,6 +43,7 @@ public class SaveNotice extends HttpServlet{
 				String RicevutoDa = req.getParameter("RicevutoDa");				
 				Boolean Stato = Boolean.valueOf(req.getParameter("Stato"));
 				Long idLocale = Long.valueOf(req.getParameter("idLocale"));
+				String Tipo = req.getParameter("Tipo");				
 
 				
 				DBConnection dbConnection = new DBConnection(); 
@@ -60,6 +61,7 @@ public class SaveNotice extends HttpServlet{
 				notice.setRicevutoDa(RicevutoDa);
 				notice.setStato(Stato);
 				notice.setIdLocale(idLocale);
+				notice.setTipo(Tipo);
 				NoticeDao.save(notice);
 					
 				
